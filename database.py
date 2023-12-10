7516738354c1fd80166c7239344cfeeba09d5c63
Arunev4wayne <153321600+Arunev4wayne@users.noreply.github.com>
2023-12-10 16:13:53 +0000

    added some more

--- database.py
+++ database.py
@@ -3,6 +3,7 @@ import os
 from sqlalchemy import create_engine, text
 
 db_connection_string=os.environ['DB_CONNECTION_STRING']
+print(os.environ.get('DB_CONNECTION_STRING'))
 engine = create_engine(
  db_connection_string, 
  connect_args={
@@ -17,3 +18,14 @@ def load_jobs_from_db():
     for row in result.all():
       jobs.append(dict(row._asdict()))
     return jobs 
+
+def load_job_from_db(job_id): 
+  with engine.connect() as conn:
+    
+    result = conn.execute(
+      text("select * from jobs where id = :id"), {'id': job_id}) 
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else: 
+      return dict(rows[0]._asdict())
